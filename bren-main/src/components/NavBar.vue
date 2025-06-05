1d7b64a5a9e31017147ec94871c20997777a1310
7fkh <155654712+7fkh@users.noreply.github.com>
2025-06-06 02:11:05 +0300

    Add files via upload

---
+++ bren-main/src/components/NavBar.vue
@@ -0,0 +1,78 @@
+<template>
+  <div class="nav">
+    <img src="https://i.ibb.co/5YZdWRq/C6-FFEB54-BC01-4292-956-F-65-A205-FF3316.png">
+    <div class="pages">
+      <a href="/">الرئيسية</a>
+      <a href="#prices">المنتجات</a>
+      <a href="/order">اطلب الان</a>
+    </div>
+    <div class="btns">
+      <a class="btn" href="https://discord.gg/bren">سيرفر الديسكورد</a>
+      <a class="btn" href="
+https://www.tiktok.com/@brenshop0?_t=8ml5v0rDaS8&_r=1">حساب التيكتوك</a>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+.nav{
+  width:100%;
+  height:15vh;
+  display:flex;
+  flex-direction:row-reverse;
+  justify-content:center;
+  align-items:center;
+  gap:20%;
+}
+
+.nav .pages{
+  display:flex;
+  flex-direction:row-reverse;
+  justify-content:center;
+  align-items:center;
+  gap:30px;
+}
+
+.nav .pages a{
+  text-decoration:none;
+  color:white;
+  cursor:pointer;
+}
+
+.nav img{
+  width:80px;
+  height:100px;
+}
+
+/*
+.nav .pages a{
+  text-decoration:none;
+  color:#3C6CFF;
+  padding:5px 15px;
+  border-top-left-radius:25px;
+  border-bottom-right-radius:25px;
+  background-color:white;
+  cursor:pointer;
+  transition:1s;
+}*/
+
+.nav .pages a:hover{
+  border-bottom:1px solid white;
+}
+
+.nav .btn{
+  text-decoration:none;
+  color:#525359;
+  padding:5px 15px;
+  border-radius:8px;
+  background-color:white;
+  cursor:pointer;
+  margin:0 10px;
+}
+
+@media screen and (max-width: 768px) {
+  .nav{
+    flex-direction:column;
+  }
+}
+</style>
---
+++ bren-main/src/views/Products.vue
@@ -0,0 +1,59 @@
+<script>
+import NavBar from '../components/NavBar.vue'
+import ProductsPlace from '../components/ProductsPlace.vue'
+import Footer from '../components/Footer.vue'
+
+export default{
+  components:{
+    NavBar,
+    ProductsPlace,
+    Footer,
+  },
+  mounted(){
+    this.$router.push({ path: '/products', query: { id: localStorage.getItem('id') }})
+  },
+}
+</script>
+
+<template>
+  <NavBar />
+  <div class="more">
+    <ProductsPlace />
+  </div>
+  <Footer />
+</template>
+
+<style scoped>
+.more{
+  margin-top:150px;
+  width:100%;
+  display:flex;
+  flex-direction:column;
+  justify-content:center;
+  align-items:center;
+}
+
+.start{
+  position:relative;
+  color:#00E84D;
+  padding:0 20px;
+  margin-bottom:100px;
+}
+
+.start::before{
+  content:'';
+  position:absolute;
+  right:0;
+  bottom:8%;
+  background-color:#00E84D;
+  padding:25px 3px;
+}
+
+.button{
+    background-color:orangered;
+    color:white;
+    padding:10px 30px;
+    border-radius:4px;
+    cursor:pointer;
+}
+</style>
